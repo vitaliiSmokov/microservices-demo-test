4be9bb6ec91395971fa2424c80d59ebf3a426eff
vitalii_smokov <vitalik23@ukr.net>
2018-11-11 17:43:09 +0200

    Created test for user registration

--- api-test/src/test/java/com/socks/tests/brutal/UserManagerTest.java
+++ api-test/src/test/java/com/socks/tests/brutal/UserManagerTest.java
@@ -1,19 +1,44 @@
 package com.socks.tests.brutal;
 
 import com.socks.api.model.User;
+import com.socks.api.services.UserApiService;
 import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+
 public class UserManagerTest {
 
+    private UserApiService userApiService = new UserApiService();
+
     @BeforeAll
-    void setUp() {
-        RestAssured.baseURI = "";
+    static void setUp() {
+        RestAssured.baseURI = "http://68.183.67.80/";
     }
 
     @Test
     void testCanRegisterUserWithValidCredentials() {
-        User user = new User();
+
+//        given
+        User user = new User()
+                .setFirstName("Vitalii")
+                .setLastName("Smokov")
+                .setUsername(RandomStringUtils.randomAlphanumeric(6))
+                .setEmail("test@mail.com")
+                .setPassword("test123");
+
+//        when
+        ValidatableResponse response = userApiService
+                .registerUser(user);
+
+//        then
+        response
+                .assertThat()
+                .statusCode(200)
+                .body("id", not(isEmptyString()));
     }
 }
