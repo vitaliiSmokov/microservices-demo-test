8eba54e0ff987846d6612452d69e625a9a68f082
vitalii_smokov <vitalik23@ukr.net>
2018-11-25 10:53:52 +0200

    Created new test with custom conditions

--- api-test/src/test/java/com/socks/tests/brutal/UserManagerTest.java
+++ api-test/src/test/java/com/socks/tests/brutal/UserManagerTest.java
@@ -1,17 +1,22 @@
 package com.socks.tests.brutal;
 
+import com.socks.api.conditions.Conditions;
 import com.socks.api.model.User;
 import com.socks.api.services.UserApiService;
 import io.restassured.RestAssured;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.socks.api.conditions.Conditions.body;
+import static com.socks.api.conditions.Conditions.statusCode;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
 
-public class UserManagerTest {
+class UserManagerTest {
 
     private UserApiService userApiService = new UserApiService();
 
@@ -32,13 +37,35 @@ public class UserManagerTest {
                 .setPassword("test123");
 
 //        when
-        ValidatableResponse response = userApiService
-                .registerUser(user);
-
-//        then
-        response
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .filters(
+                        new RequestLoggingFilter(),
+                        new ResponseLoggingFilter()
+                ).body(user)
+                .when()
+                .post("register")
+//                Then
+                .then()
                 .assertThat()
                 .statusCode(200)
                 .body("id", not(isEmptyString()));
+    }
+
+    @Test
+    void testCanRegisterUserWithInValidCredentials() {
+
+        User user = new User()
+                .setFirstName("Vitalii")
+                .setLastName("Smokov")
+                .setUsername(RandomStringUtils.randomAlphanumeric(6))
+                .setEmail("test@mail.com")
+                .setPassword("test123");
+
+        userApiService
+                .registerUser(user)
+                .shouldHave(statusCode(200))
+                .shouldHave(body("id", not(isEmptyString())));
     }
 }
