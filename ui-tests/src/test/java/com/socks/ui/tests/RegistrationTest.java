f6c03b76cd1fc307b42fe30d759375a88e5a23b1
Vitalii Smokov <vitalii.smokov@globaltechmakers.com>
2018-12-14 17:30:16 +0200

    allure version is updated to 2.7

--- ui-tests/src/test/java/com/socks/ui/tests/RegistrationTest.java
+++ ui-tests/src/test/java/com/socks/ui/tests/RegistrationTest.java
@@ -1,8 +1,10 @@
 package com.socks.ui.tests;
 
 import com.socks.ui.pages.MainPage;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
+@Slf4j
 public class RegistrationTest extends BaseTest{
 
     @Test
