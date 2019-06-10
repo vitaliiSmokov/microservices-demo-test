6fef8b5db4fec4dcb84f877296db21196636af6e
Vitalii Smokov <vitalii.smokov@globaltechmakers.com>
2019-06-10 11:27:12 +0300

    Added custom serializes

---
+++ ui-tests/src/main/java/com/socks/ui/constants/FileTypes.java
@@ -0,0 +1,12 @@
+package com.socks.ui.constants;
+
+/**
+ * @author Vitalii Smokov 24.04.2019
+ */
+public class FileTypes {
+
+  public static final String IMAGE_PNG = "image/png";
+  public static final String IMAGE_JPG = "image/jpg";
+  public static final String FILE_PDF = "application/pdf";
+  public static final String FILE_JSON = "application/json";
+}
---
+++ ui-tests/src/main/java/com/socks/ui/constants/Locales.java
@@ -0,0 +1,9 @@
+package com.socks.ui.constants;
+
+/**
+ * @author Vitalii Smokov 11.04.2019
+ */
+public enum Locales {
+  EN,
+  GE
+}
