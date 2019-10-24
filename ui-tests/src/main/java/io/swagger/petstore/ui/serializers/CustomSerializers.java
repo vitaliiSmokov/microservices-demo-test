c63d92bc863e03a3db2001bcab0d2e73a0f884c3
Vitalii Smokov <vitalii.smokov@globaltechmakers.com>
2019-10-24 17:22:17 +0300

    Fixed compile error

--- ui-tests/src/main/java/io/swagger/petstore/ui/serializers/CustomSerializers.java
+++ ui-tests/src/main/java/io/swagger/petstore/ui/serializers/CustomSerializers.java
@@ -1,15 +1,22 @@
 package io.swagger.petstore.ui.serializers;
 
-import static java.time.format.DateTimeFormatter.*;
-
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.databind.*;
-import com.socks.ui.constants.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import io.swagger.petstore.ui.constants.DatePatternConstant;
 import io.swagger.petstore.ui.constants.FormatConstants;
 
-import java.io.*;
-import java.time.*;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 
 public class CustomSerializers {
@@ -20,7 +27,7 @@ public class CustomSerializers {
             gen.writeString(value.toString());
         }
     }
-    
+
     public static class LongDeserializer extends JsonDeserializer<Long> {
         @Override
         public Long deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
@@ -29,12 +36,12 @@ public class CustomSerializers {
     }
 
     public static class ZonedDateTimeAsPlainDateDeserializer extends
-        JsonDeserializer<ZonedDateTime> {
+            JsonDeserializer<ZonedDateTime> {
         @Override
         public ZonedDateTime deserialize(
-            JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+                JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
             LocalDate parsedLocalDate = LocalDate.parse(p.getValueAsString(), FormatConstants.DATE_FORMATTER);
-            return ZonedDateTime.of(parsedLocalDate.atTime(0,0), ZoneId.of("UTC"));
+            return ZonedDateTime.of(parsedLocalDate.atTime(0, 0), ZoneId.of("UTC"));
         }
     }
 
@@ -51,13 +58,13 @@ public class CustomSerializers {
         @Override
         //2001-12-17T00:00:00.000+0000"
         public ZonedDateTime deserialize(
-            JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+                JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
             String valueAsString = p.getValueAsString();
             if (valueAsString.matches("^\\d{4}(-\\d{2}){2}T(\\d{2}:?){3}\\.\\d{3}\\+\\d{4}$")) {
                 String[] timeStamp = valueAsString.split("T");
-                return ZonedDateTime.parse(timeStamp[0],FormatConstants.DATE_TIME_ISO_UTC);
+                return ZonedDateTime.parse(timeStamp[0], FormatConstants.DATE_TIME_ISO_UTC);
             } else {
-                return ZonedDateTime.parse(valueAsString,FormatConstants.DATE_TIME_ISO_UTC);
+                return ZonedDateTime.parse(valueAsString, FormatConstants.DATE_TIME_ISO_UTC);
             }
         }
     }
@@ -72,8 +79,8 @@ public class CustomSerializers {
     public static class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
         @Override
         public void serialize(LocalDateTime value, JsonGenerator gen,
-            SerializerProvider serializers)
-            throws IOException {
+                              SerializerProvider serializers)
+                throws IOException {
             gen.writeString(value.format(ofPattern(DatePatternConstant.DATE_TIME_SECONDS_PATTERN)));
         }
     }
--- ui-tests/src/main/java/utils/MyDriverManager.java
+++ ui-tests/src/main/java/utils/MyDriverManager.java
@@ -1,12 +1,8 @@
 package utils;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.DriverManagerType;
 import io.qameta.allure.Step;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.Objects;
-
 import io.swagger.petstore.ui.pages.enums.Browser;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
@@ -14,58 +10,64 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Objects;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.getInstance;
+
 /**
  * @author Vitalii Smokov 18.01.2019
  */
 public class MyDriverManager {
 
-  @Step("Init remote selenoid Chrome driver")
-  public static WebDriver initChromeDriver() {
-    return getRemoteWebDriver(getSelenoidCapabilities(Browser.CHROME, "69.0"));
-  }
-
-  @Step("Init remote selenoid Firefox driver")
-  public static WebDriver initFirefoxDriver() {
-    return getRemoteWebDriver(getSelenoidCapabilities(Browser.FIREFOX, "63.0"));
-  }
-
-  @Step("Init remote selenoid Chrome driver with locale: {locale}")
-  public static WebDriver initChromeDriverWithLocale(String locale) {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--lang=" + locale);
-    DesiredCapabilities browser = getSelenoidCapabilities(Browser.CHROME, "70.0");
-    browser.setCapability(ChromeOptions.CAPABILITY, options);
-    return getRemoteWebDriver(browser);
-  }
-
-
-  private static DesiredCapabilities getSelenoidCapabilities(Browser browser, String version) {
-    Configuration.browser = browser.toString().toLowerCase();
-    ChromeDriverManager.getInstance().setup();
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setBrowserName(browser.toString().toLowerCase());
-    capabilities.setVersion(version);
-    capabilities.setCapability("enableVNC", true);
-    capabilities.setCapability("enableVideo", false);
-    return capabilities;
-  }
-
-  private static WebDriver getRemoteWebDriver(DesiredCapabilities browser) {
-    RemoteWebDriver driver = null;
-    try {
-      driver = new RemoteWebDriver(
-          URI.create("http://3.121.85.141:4444/wd/hub").toURL(),
-          browser
-      );
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
+    @Step("Init remote selenoid Chrome driver")
+    public static WebDriver initChromeDriver() {
+        return getRemoteWebDriver(getSelenoidCapabilities(Browser.CHROME, "69.0"));
     }
-    setDimension(driver, new Dimension(1920, 1080));
-    return driver;
-  }
 
-  private static void setDimension(RemoteWebDriver driver, Dimension dimension) {
-    Objects.requireNonNull(driver).manage().window().setSize(dimension);
-  }
+    @Step("Init remote selenoid Firefox driver")
+    public static WebDriver initFirefoxDriver() {
+        return getRemoteWebDriver(getSelenoidCapabilities(Browser.FIREFOX, "63.0"));
+    }
+
+    @Step("Init remote selenoid Chrome driver with locale: {locale}")
+    public static WebDriver initChromeDriverWithLocale(String locale) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--lang=" + locale);
+        DesiredCapabilities browser = getSelenoidCapabilities(Browser.CHROME, "70.0");
+        browser.setCapability(ChromeOptions.CAPABILITY, options);
+        return getRemoteWebDriver(browser);
+    }
+
+
+    private static DesiredCapabilities getSelenoidCapabilities(Browser browser, String version) {
+        Configuration.browser = browser.toString().toLowerCase();
+        getInstance(DriverManagerType.CHROME).setup();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser.toString().toLowerCase());
+        capabilities.setVersion(version);
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        return capabilities;
+    }
+
+    private static WebDriver getRemoteWebDriver(DesiredCapabilities browser) {
+        RemoteWebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(
+                    URI.create("http://3.121.85.141:4444/wd/hub").toURL(),
+                    browser
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        setDimension(driver, new Dimension(1920, 1080));
+        return driver;
+    }
+
+    private static void setDimension(RemoteWebDriver driver, Dimension dimension) {
+        Objects.requireNonNull(driver).manage().window().setSize(dimension);
+    }
 
 }
