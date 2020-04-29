edae54dca1561dd7111edf3f9bc1422a7025e972
Vitalii Smokov <vitalii.smokov@globaltechmakers.com>
2020-04-29 16:36:05 +0300

    Add remove exception

--- api-test/src/main/java/io/swagger/petstore/api/serializers/StringToDateDeserializer.java
+++ api-test/src/main/java/io/swagger/petstore/api/serializers/StringToDateDeserializer.java
@@ -1,7 +1,6 @@
 package io.swagger.petstore.api.serializers;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import io.swagger.petstore.api.utils.DateTimeUtil;
@@ -11,11 +10,10 @@ import java.time.LocalDate;
 
 public class StringToDateDeserializer extends JsonDeserializer<LocalDate> {
 
-    @Override
-    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt)
-            throws IOException, JsonProcessingException {
+  @Override
+  public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
 
-        String value = p.readValueAs(String.class);
-        return DateTimeUtil.toLocalDate(value);
-    }
+    String value = p.readValueAs(String.class);
+    return DateTimeUtil.toLocalDate(value);
+  }
 }
