6dd42165a00d695308e3b835dd791e8566572ddc
smokov <vitaliy.smokov@fuib.com>
2020-11-24 14:40:38 +0200

    Removed @UtilityClass for the BaseGenerator

--- api-test/src/main/java/io/swagger/petstore/api/generators/BaseGenerator.java
+++ api-test/src/main/java/io/swagger/petstore/api/generators/BaseGenerator.java
@@ -4,9 +4,7 @@ import com.devskiller.jfairy.producer.person.Person;
 import com.github.javafaker.Faker;
 import io.swagger.petstore.api.utils.FakerUtil;
 import io.swagger.petstore.api.utils.RandomUtil;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class BaseGenerator {
   protected static final Faker FAKER = FakerUtil.getFaker();
   protected static final Person PERSON = RandomUtil.getFairy().person();
